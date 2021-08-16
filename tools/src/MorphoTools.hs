24a55d003417fa01a4f7bd531bddbc73f8f3e3eb
Ana <analuizanunes8@gmail.com>
2021-08-16 16:46:48 -0300

    delete

--- tools/src/MorphoTools.hs
+++ tools/src/MorphoTools.hs
@@ -10,7 +10,7 @@ import System.FilePath.Posix
 import qualified Data.Text as T
 import qualified Data.Text.IO as TO
 import Data.List (groupBy, intercalate, sort, nub, sortOn)
-import Data.List.Split (splitPlaces, chunksOf)
+import Data.List.Split (splitOn, splitPlaces, chunksOf)
 import Data.Maybe ( fromJust, isNothing )
 import qualified Text.Regex as R
 
@@ -59,7 +59,8 @@ member x [] = False
 member x (y:ys) | x==y = True
                 | otherwise = member x ys
 
------- 
+------ apagar entradas 
+
 auxCheckDup :: [(T.Text, T.Text)] -> T.Text -> [T.Text]
 auxCheckDup (x:xs) tags
  | snd x == tags = (snd x) : auxCheckDup xs tags
@@ -70,4 +71,32 @@ checkDup :: M.Map T.Text [(T.Text, T.Text)] -> (T.Text,T.Text) -> Bool
 checkDup map (lema, tags)
  | length (auxCheckDup  (fromJust $ M.lookup lema map) tags) > 1 = True
  | otherwise = False
+
+delEntry :: T.Text -> T.Text -> T.Text -> M.Map T.Text [(T.Text, T.Text)] ->  M.Map T.Text [(T.Text, T.Text)]
+delEntry forma lema tags m = M.insert lema (aux forma tags $ fromJust $ M.lookup lema m) m
+ where 
+   aux forma tags (x:xs) 
+    | (forma == fst x) && (tags == snd x) = xs
+    | otherwise = x : aux forma tags xs
+   aux forma tags [] = []
+
+auxDelete :: M.Map T.Text [(T.Text, T.Text)] -> [T.Text] ->  M.Map T.Text [(T.Text, T.Text)]
+auxDelete m (x:xs) 
+    | checkDup m (T.breakOn "+" $ last $ T.splitOn "\t" x) = 
+      auxDelete (delEntry (head $ T.splitOn "\t" x) -- forma
+       (head $ T.splitOn "+" $ last $ T.splitOn "\t" x) (snd $ T.breakOn "+" x) m) xs -- lema tags
+    | otherwise = auxDelete m xs 
+auxDelete m [] = m
+
+delete :: FilePath -> FilePath -> FilePath -> IO [()]
+delete dir path outpath = do
+  paths <- listDirectory dir
+  dicts <- mapM (morphoMap . combine dir) paths
+  entries <- TO.readFile path
+  mapM (aux outpath) 
+    (splitEvery 19000 (toEntries (M.toList $ auxDelete (foldr (M.unionWith (++)) M.empty dicts) (T.lines entries))))
+ where
+    aux outpath (x:xs) =
+     TO.writeFile (combine outpath ("verbs-"++(take 7 $ T.unpack $ fst $ T.breakOn "\t" x)++".dict"))
+     (T.append (T.intercalate "\n" (x:xs)) "\n")
 
