65140eff2064674d7b4e2393a079e1b6fa14448e
Ana <analuizanunes8@gmail.com>
2021-08-25 18:58:12 -0300

    new corLemma

--- tools/src/MorphoTools.hs
+++ tools/src/MorphoTools.hs
@@ -49,8 +49,8 @@ morphoMap path = do
 
 toEntries :: [(T.Text,[(T.Text, T.Text)])] -> [T.Text]
 toEntries xs =  concatMap (\(a,b) ->  map (aux a) (nub b)) xs
- where 
-   aux lema (forma,tags) = 
+ where
+   aux lema (forma,tags) =
      T.append forma (T.append "\t" (T.append lema tags))
 
 
@@ -74,49 +74,63 @@ checkDup map (lema, tags)
 
 delEntry :: T.Text -> T.Text -> T.Text -> M.Map T.Text [(T.Text, T.Text)] ->  M.Map T.Text [(T.Text, T.Text)]
 delEntry forma lema tags m = M.insert lema (aux forma tags $ fromJust $ M.lookup lema m) m
- where 
-   aux forma tags (x:xs) 
+ where
+   aux forma tags (x:xs)
     | (forma == fst x) && (tags == snd x) = xs
     | otherwise = x : aux forma tags xs
    aux forma tags [] = []
 
 auxDelete :: M.Map T.Text [(T.Text, T.Text)] -> [T.Text] ->  M.Map T.Text [(T.Text, T.Text)]
-auxDelete m (x:xs) 
-    | checkDup m (T.breakOn "+" $ last $ T.splitOn "\t" x) = 
+auxDelete m (x:xs)
+    | checkDup m (T.breakOn "+" $ last $ T.splitOn "\t" x) =
       auxDelete (delEntry (head $ T.splitOn "\t" x) -- forma
        (head $ T.splitOn "+" $ last $ T.splitOn "\t" x) (snd $ T.breakOn "+" x) m) xs -- lema tags
-    | otherwise = auxDelete m xs 
+    | otherwise = auxDelete m xs
 auxDelete m [] = m
 
 delete :: FilePath -> FilePath -> FilePath -> IO [()]
 delete dir path outpath = do
+  let cl = takeBaseName dir
   paths <- listDirectory dir
   dicts <- mapM (morphoMap . combine dir) paths
   entries <- TO.readFile path
-  mapM (aux outpath) 
+  mapM (aux cl outpath)
     (splitEvery 19000 (toEntries (M.toList $ auxDelete (foldr (M.unionWith (++)) M.empty dicts) (T.lines entries))))
  where
-    aux outpath (x:xs) =
-     TO.writeFile (combine outpath ("verbs-"++(take 7 $ T.unpack $ fst $ T.breakOn "\t" x)++".dict"))
+    aux cl outpath (x:xs) =
+     TO.writeFile (combine outpath (cl++(take 7 $ T.unpack $ fst $ T.breakOn "\t" x)++".dict"))
      (T.append (T.intercalate "\n" (x:xs)) "\n")
 
 
 ---- corrigir lema
 
-auxCorLemma :: (String,String) -> M.Map T.Text [(T.Text, T.Text)] -> M.Map T.Text [(T.Text, T.Text)]
-auxCorLemma (del,new) m 
- | M.member (T.pack del) m = M.delete (T.pack del)
-      (M.insertWith (++) (T.pack new) (fromJust $ M.lookup (T.pack del) m) m)
- | otherwise = m
+getPath :: String -> [String] -> String -> String
+getPath cl (x:y:xs) l 
+ |(x <= (cl ++ (take 7 l) ++ ".dict")) && ((cl ++ (take 7 l) ++ ".dict") < y) = x
+ | otherwise = getPath cl (y:xs) l
+getPath cl (y:[]) l = y
 
--- (errada,certa)
-corLemma :: FilePath -> (String, String) -> FilePath -> IO [()]
-corLemma vdir ls outpath = do
-  dpaths <- listDirectory vdir
-  dicts <- mapM (morphoMap . combine vdir) dpaths
-  mapM (aux outpath) 
-    (splitEvery 19000 (toEntries $ M.toList $ foldr (M.unionWith (++)) M.empty (map (auxCorLemma ls) dicts)))
+auxCorLemma :: (String, String) -> [M.Map T.Text [(T.Text, T.Text)]] -> [[T.Text]]
+auxCorLemma (del,new) [m] = [toEntries $ M.toList $
+    M.delete (T.pack del) (M.insertWith (++) (T.pack new) (fromJust $ M.lookup (T.pack del) m) m)]
+auxFunc (del,new) (x:y:xs)
+ | M.member (T.pack del) x = map (toEntries . M.toList)
+    [M.delete (T.pack del) x, M.insertWith (++) (T.pack new) (fromJust $ M.lookup (T.pack del) x) y]
+ | otherwise = map (toEntries . M.toList)
+    [M.insertWith (++) (T.pack new) (fromJust $ M.lookup (T.pack del) y) x, M.delete (T.pack del) y]
+
+-- (errado,certo)
+corLemma :: FilePath -> (String,String) -> IO [()]
+corLemma dir (del,new) = do
+  let cl = takeBaseName dir
+  paths <- listDirectory dir
+  dicts <- mapM (morphoMap . combine dir) (map (getPath cl paths) [del, new])
+  mapM (aux dir cl paths) (auxCorLemma (del,new) dicts)
  where
-    aux outpath (x:xs) =
-     TO.writeFile (combine outpath ("verbs-"++(take 7 $ T.unpack $ fst $ T.breakOn "\t" x)++".dict"))
-     (T.append (T.intercalate "\n" (x:xs)) "\n")
+    aux dir cl paths (x:xs) 
+     | member (cl++(take 7 $ T.unpack $ fst $ T.breakOn "\t" x)++".dict") paths =
+      TO.writeFile (combine dir (cl++(take 7 $ T.unpack $ fst $ T.breakOn "\t" x)++".dict"))
+      (T.append (T.intercalate "\n" (x:xs)) "\n")
+     | otherwise = do
+      removeFile (combine dir (getPath cl paths (T.unpack x)))
+      TO.writeFile (combine dir (cl++(take 7 $ T.unpack $ fst $ T.breakOn "\t" x)++".dict")) (T.append (T.intercalate "\n" (x:xs)) "\n")
